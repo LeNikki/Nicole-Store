f3f702465897a095f583c9c5e788f4d4e5bc7ab6
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2022-09-25 18:33:41 +0800

    Updates

---
+++ precache-manifest.70493a3f76219b771f4627adf2ca98b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d935c5c1d3a987abc3",
+    "url": "/static/css/main.426715c8.chunk.css"
+  },
+  {
+    "revision": "13d935c5c1d3a987abc3",
+    "url": "/static/js/main.13d935c5.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "639773caf3595b75135c",
+    "url": "/static/js/2.639773ca.chunk.js"
+  },
+  {
+    "revision": "6ee34327ee87b8f52b89",
+    "url": "/static/js/runtime~main.6ee34327.js"
+  },
+  {
+    "revision": "e2f297aa3f3e45bc2ca7c91eefcaa79d",
+    "url": "/static/media/mainpic.e2f297aa.png"
+  },
+  {
+    "revision": "f151585587665e457edffd0bd5b51353",
+    "url": "/static/media/prod1.f1515855.png"
+  },
+  {
+    "revision": "ab7880449fb632f0a80f814af5bf978c",
+    "url": "/static/media/prod2.ab788044.png"
+  },
+  {
+    "revision": "96ea66ba12aeaf85e73e9aac7d2a320b",
+    "url": "/static/media/prod3.96ea66ba.png"
+  },
+  {
+    "revision": "423b1a65576b67790d3d08eca5252426",
+    "url": "/index.html"
+  }
+];
