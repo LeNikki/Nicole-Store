75a97eec2281659ca20059a8d04aff68ff938893
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2022-09-26 08:31:03 +0800

    5th commit

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import Home from "./Components/Home"
 import Item from "./Components/node"
 import Footer from "./Components/Footer"
 import { products } from './Components/products';
+import React from "react"
+
 function App() {
   const node = products.map((i)=>{
     return <Item key={i.id} {...i}/>
   })
+
   return (
     <div className="App">
       <Header/>
       <div className='container'>
       <Home/>
-      <div className='node-container'>
-        {node}
-      </div>
+        <div className='node-container'>
+          {node}
+        </div>
       </div>
       <Footer/>
     </div>
