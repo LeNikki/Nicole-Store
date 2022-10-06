649cad4627fcaf58010678a81b17e8cb10fcd75e
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2022-10-06 22:52:46 +0800

    changed className

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,9 +5,9 @@ export default function Footer(){
         <div className="footer-cont">
             <h3>Follow us on Social Media</h3>
             <section className="socials">
-              <a href="#"><i class="fa-brands fa-facebook"></i></a>
-              <a href="#"><i class="fa-brands fa-instagram"></i></a>
-              <a href="#"><i class="fa-brands fa-github"></i></a> 
+              <a href="#"><i className="fa-brands fa-facebook"></i></a>
+              <a href="#"><i className="fa-brands fa-instagram"></i></a>
+              <a href="#"><i className="fa-brands fa-github"></i></a> 
             </section>
             <p>Copyright Nicole Satiembre 2022. All rights reserved.</p>
         </div>
--- src/Components/node.js
+++ src/Components/node.js
@@ -6,7 +6,7 @@ export default function Item (props){
             <img src= {props.img} alt="prod-img" className="prod-img"/>
             <section className="hotPicks-info">
                 <h2 className="prod-name">{props.name}</h2>
-                <p className="rating"><i class="fa-solid fa-heart"></i> {props.rating}</p>
+                <p className="rating"><i className="fa-solid fa-heart"></i> {props.rating}</p>
                 <p className="price">{props.price}</p>
                 <button className="add-to-cart"> Add to cart </button>
             </section>
